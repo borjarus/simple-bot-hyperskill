eea88499d3ce1f2f4bf57be784ef6cce21e570fe
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-12 14:23:04 +0100

    stage 2

--- Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -1,5 +1,22 @@
 package bot
 
 fun main() {
-    // write your code here
+    val botName = "Aid"
+    val birthYear = 2020
+
+    println("Hello! My name is $botName.")
+    println("I was created in $birthYear.")
+
+    println("Please, remind me your name.")
+    val name = readln()
+    println("What a great name you have, $name!")
+
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val (r3, r5, r7) = readln().split(" ").map { it.toInt() }
+    val age = (r3 * 70 + r5 * 21 + r7 * 15) % 105
+
+    println("Your age is $age; that's a good time to start programming!")
+
 }
