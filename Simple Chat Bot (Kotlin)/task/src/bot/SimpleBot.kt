a49424eb82bdbef0952b8e3cbb40311c38d8a6d0
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-12 15:17:37 +0100

    stage 3

--- Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -18,5 +18,38 @@ fun main() {
     val age = (r3 * 70 + r5 * 21 + r7 * 15) % 105
 
     println("Your age is $age; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
 
+    val n = readln().toInt()
+    (0..n).forEach { println("$it!") }
+
+    println("Completed, have a nice day!")
 }
+
+/*
+fun main() {
+    val botName = "Aid"
+    val birthYear = 2020
+
+    println("Hello! My name is $botName.")
+    println("I was created in $birthYear.")
+    println("Please, remind me your name.")
+
+    val name = readln()
+    println("What a great name you have, $name!")
+
+    println("Let me guess your age.")
+    println("Say me remainders of dividing your age by 3, 5 and 7.")
+
+    val (r3, r5, r7) = readln().split(" ").map(String::toInt)
+    val age = (r3 * 70 + r5 * 21 + r7 * 15) % 105
+
+    println("Your age is $age; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    val n = readln().toInt()
+    (0..n).forEach { println("$it!") }
+
+    println("Completed, have a nice day!")
+}
+ */
